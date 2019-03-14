32e0aaf13f25dd032a3a9491e28c54e849842704
kouki.yamamura <kouki0218@outlook.jp>
2019-03-14 15:34:00 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,21 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   before_action :set_task,only:[:show,:edit,:update,:destroy]
     def index
-        @task =Task.all.order(created_at: :desc).page(params[:page]).per(10)
+      if logged_in?
+       @task =current_user.tasks.order(created_at: :desc).page(params[:page]).per(10)
+      end
     end
     
     def show
     end
     
     def new
-        @task =Task.new
+        @task =current_user.tasks.build
     end
     
     def create
-        @task =Task.new(task_params)
+        @task =current_user.tasks.build(task_params)
         
         if @task.save
           flash[:success]='タスクが正常に投稿されました'
@@ -54,4 +57,9 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content,:status)
     end
+    def require_user_logged_in
+    unless logged_in?
+      redirect_to signup_url
+    end
+  end
 end
